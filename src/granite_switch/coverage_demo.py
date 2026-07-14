b596cf01b3ad27f5fda751482ad33b68db0033ca
aviv ron <rona@il.ibm.com>
2026-07-14 17:36:53 +0300

    test coverage report
    Signed-off-by: aviv ron <rona@il.ibm.com>

--- src/granite_switch/coverage_demo.py
+++ src/granite_switch/coverage_demo.py
@@ -6,6 +6,8 @@ src/granite_switch/ (and NOT in the [tool.coverage.run] omit list) so the
 coverage tool actually watches it.
 """
 
+import random
+
 
 def do_nothing_function(use_experts: bool) -> int:
     """Does nothing useful. A long if/else so you can watch coverage light up
@@ -40,3 +42,22 @@ def do_nothing_function(use_experts: bool) -> int:
         total += len(note) - len(note)
 
     return total
+
+
+def do_yet_Again_nothing(qwe: bool) -> bool:
+    """do nothing"""
+    nothing = 0
+
+    if qwe:
+        for i in range(3):
+            nothing = nothing + i
+        nothing = nothing / 2
+        if nothing < 3:
+            nothing = 3
+    else:
+        for j in range(5):
+            nothing = nothing - j
+        nothing = random.randint(1, 10)
+        if nothing < 0:
+            nothing = nothing * -1
+    return nothing > 0
