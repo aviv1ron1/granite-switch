037d0c7648a5dc98de39b27e7399e16d5056efa5
aviv ron <rona@il.ibm.com>
2026-07-16 12:51:58 +0300

    test ruff
    Signed-off-by: aviv ron <rona@il.ibm.com>

--- src/granite_switch/config.py
+++ src/granite_switch/config.py
@@ -78,6 +78,8 @@ class GraniteSwitchConfig(GraniteMoeHybridConfig):
             layer_types=layer_types,
             **kwargs,
         )
+        x = 1
+        num_adapters = x
 
         # Default shared_intermediate_size from intermediate_size.
         # All Granite 4 models use shared_mlp naming; for dense models
